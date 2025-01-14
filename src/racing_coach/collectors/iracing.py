10478df4b23f721b2374846f2757a41e09dd48f3
Sawyer <sawyerczupka@gmail.com>
2025-01-14 18:54:35 -0500

    refactor: clean up unused event subscription methods in TelemetryCollector

--- src/racing_coach/collectors/iracing.py
+++ src/racing_coach/collectors/iracing.py
@@ -16,10 +16,6 @@ from .connection import iRacingConnectionManager
 logger = logging.getLogger(__name__)
 
 
-# EventType = Literal["lap_completed"]  # Add more events here
-LapCallback: TypeAlias = Callable[[LapTelemetry], None]
-
-
 class TelemetryCollector:
     def __init__(self, event_bus: EventBus):
         """Initialize the telemetry collector."""
@@ -29,25 +25,7 @@ class TelemetryCollector:
         self.running: bool = False
 
         self.data_buffer: list[TelemetryFrame] = []
-        # self.data_buffer2: list[TelemetryFrame] = []
         self.last_lap_number = -1
-
-    # def subscribe(self, event_type: EventType, callback: LapCallback) -> None:
-    #     """Subscribe to events."""
-    #     self._subscribers[event_type].append(callback)
-
-    # def unsubscribe(self, event_type: EventType, callback: LapCallback) -> None:
-    #     """Unsubscribe from events."""
-    #     if callback in self._subscribers[event_type]:
-    #         self._subscribers[event_type].remove(callback)
-
-    # def notify_subscribers(self, event_type: EventType, data: Any) -> None:
-    #     """Notify subscribers of a lap completion."""
-    #     for subscriber in self._lap_subscribers[event_type]:
-    #         try:
-    #             subscriber(data)
-    #         except Exception as e:
-    #             logger.error(f"Error in lap subscriber callback: {e}")
 
     def collect_frame(self):
         """Collect a single frame of telemetry data."""
@@ -66,18 +44,13 @@ class TelemetryCollector:
         if frame.lap_number != self.last_lap_number:
             logger.info(f"New lap: {frame.lap_number}")
             # New lap, save and reset the buffer.
-
-            lap_frames = deepcopy(self.data_buffer)
-            lap_telemetry = LapTelemetry(frames=lap_frames, lap_time=None)
-
-            self.event_bus.thread_safe_publish(
-                Event(EventType.LAP_COMPLETED, data=lap_telemetry)
-            )
-
             if self.last_lap_number != -1:
-                last_time = (
-                    frame.last_lap_time
-                )  # TODO: fix this, last lap isn't always the last lap
+                lap_frames = deepcopy(self.data_buffer)
+                lap_telemetry = LapTelemetry(frames=lap_frames, lap_time=None)
+
+                self.event_bus.thread_safe_publish(
+                    Event(EventType.LAP_COMPLETED, data=lap_telemetry)
+                )
 
             self.last_lap_number = frame.lap_number
             self.data_buffer = []
