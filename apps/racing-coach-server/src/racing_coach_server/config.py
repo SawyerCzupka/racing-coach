5aee82b35544d9959ecbcd30a8b439bbe9face0b
Sawyer <sawyerczupka@gmail.com>
2025-04-28 10:41:05 -0400

    feat: add configuration settings for database connection using Pydantic

---
+++ apps/racing-coach-server/src/racing_coach_server/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_CONNECTION_STR: str = (
+        "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+    )
+
+
+settings = Settings()
