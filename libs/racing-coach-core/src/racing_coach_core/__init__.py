11f7ba5fd7b13cc1ab62c63bc7834489aa11a99c
Sawyer <sawyerczupka@gmail.com>
2025-06-04 23:13:48 -0400

    feat: initialize models module and define public API for telemetry and response models

--- libs/racing-coach-core/src/racing_coach_core/__init__.py
+++ libs/racing-coach-core/src/racing_coach_core/__init__.py
@@ -1,2 +1,18 @@
+from .client import RacingCoachClient, RacingCoachClientError, ServerError, RequestError
+from .models import LapTelemetry, SessionFrame, TelemetryFrame, HealthCheckResponse, LapUploadResponse
+
 def hello() -> str:
     return "Hello from racing-coach-core!"
+
+__all__ = [
+    "RacingCoachClient", 
+    "RacingCoachClientError", 
+    "ServerError", 
+    "RequestError", 
+    "LapTelemetry",
+    "SessionFrame", 
+    "TelemetryFrame",
+    "HealthCheckResponse",
+    "LapUploadResponse",
+    "hello"
+]
---
+++ libs/racing-coach-core/src/racing_coach_core/models/__init__.py
@@ -0,0 +1,10 @@
+from .telemetry import LapTelemetry, SessionFrame, TelemetryFrame
+from .responses import HealthCheckResponse, LapUploadResponse
+
+__all__ = [
+    "LapTelemetry",
+    "SessionFrame", 
+    "TelemetryFrame",
+    "HealthCheckResponse",
+    "LapUploadResponse",
+]
